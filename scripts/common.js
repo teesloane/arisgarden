34b316d948ed316b3450986ec6620e1a3073aeef
teesloane <tylersloane@gmail.com>
2019-09-19 19:08:47 -0400

    Prettify.

--- scripts/common.js
+++ scripts/common.js
@@ -6,12 +6,11 @@ function v_Hero(bgImage) {
     style: { backgroundImage: `url(${bgImage})` }
   };
 
-  return h("section", attrs );
+  return h("section", attrs);
 }
 
-
 function v_LargeText(t) {
-  return h("h1", {class: "v_LargeText"}, t)
+  return h("h1", { class: "v_LargeText" }, t);
 }
 
 function v_GiantQuote(t) {
@@ -26,30 +25,33 @@ function v_GiantQuote(t) {
 }
 
 function v_Icon(name) {
-  return h("img", {src: `media/icons/${name}`})
+  return h("img", { src: `media/icons/${name}` });
 }
-
 
 /**
  * Renders a timer, absolutely positioned at the bottom of the screen.
  */
 function v_Timer(state) {
-  let t = state.timer ? time.secToStr(state.timer) : "00:00:00"
-  let centerStyle = {style: {alignSelf: "center", display: "flex"}}
-  let stepText = state.currentRecipeStepText
-  let _class = state.timerRunning ? "v_TimerFixed" : "v_TimerFixed v_TimerFixed_hidden"
+  let t = state.timer ? time.secToStr(state.timer) : "00:00:00";
+  let centerStyle = { style: { alignSelf: "center", display: "flex" } };
+  let stepText = state.currentRecipeStepText;
+  let _class = state.timerRunning
+    ? "v_TimerFixed"
+    : "v_TimerFixed v_TimerFixed_hidden";
 
-  let v_CloseBtn = () => h("span", {class: "v_TimerFixed_closeBtn", onClick: [actions.cancelTimer]}, v_Icon("x-circle-wh.svg") )
+  let v_CloseBtn = () =>
+    h(
+      "span",
+      { class: "v_TimerFixed_closeBtn", onClick: [actions.cancelTimer] },
+      v_Icon("x-circle-wh.svg")
+    );
 
-  return h(
-    "div", {class: _class},[
-      h("span", centerStyle, [
-        h("span", {class: "pr1"}, v_Icon("watch_wh.svg")),
-        h("span", {class: "pr4", style: {alignSelf: "center"}}, `${t}`),
-      ]),
-      h("span", {style: {alignSelf: "center"}}, `${stepText}`),
-      v_CloseBtn()
-    ]
-  )
-
+  return h("div", { class: _class }, [
+    h("span", centerStyle, [
+      h("span", { class: "pr1" }, v_Icon("watch-wh.svg")),
+      h("span", { class: "pr4", style: { alignSelf: "center" } }, `${t}`)
+    ]),
+    h("span", { style: { alignSelf: "center" } }, `${stepText}`),
+    v_CloseBtn()
+  ]);
 }
--- scripts/convert.js
+++ scripts/convert.js
@@ -1,9 +1,9 @@
-const { parse } = require('/usr/local/lib/node_modules/orga')
+const { parse } = require("/usr/local/lib/node_modules/orga");
 
-var fs = require('fs');
+var fs = require("fs");
 var db = null;
 
-fs.readFile("recipes.org", 'utf8', function(_, data) {
+fs.readFile("recipes.org", "utf8", function(_, data) {
   db = parse(data);
   let recipes = db.children;
   let output = {
@@ -12,16 +12,17 @@ fs.readFile("recipes.org", 'utf8', function(_, data) {
 
   recipes.forEach(r => {
     let _r = getRecipe(r);
-    let key = _r.meta.properties.slug
-    output.recipes[key] = _r
-  })
+    let key = _r.meta.properties.slug;
+    output.recipes[key] = _r;
+  });
   let stringify = "var db = " + JSON.stringify(output, null, 2);
 
   fs.writeFile("./db.js", stringify, function(err) {
-    if (err) { return console.log(err); }
+    if (err) {
+      return console.log(err);
+    }
     console.log("The file was saved!");
   });
-
 });
 
 function getRecipe(heading) {
@@ -32,25 +33,26 @@ function getRecipe(heading) {
     },
     ingredients: getIngredients(heading),
     instructions: getInstructions(heading)
-  }
+  };
 }
 
-
 function getIngredients(n) {
-  let recipe = n
+  let recipe = n;
   let ingredients = tableParser(recipe.children[1].children[1].children);
   return ingredients;
 }
 
 // Returns a recipes instructions
 function getInstructions(n) {
-  let list = n.children[2].children[1].children
-  let out = []
-  list.forEach(l => l.children.map(j => {
-    out.push(templateParser(j.value))
-    // return templateParser(j.value);
-  }))
-  return out
+  let list = n.children[2].children[1].children;
+  let out = [];
+  list.forEach(l =>
+    l.children.map(j => {
+      out.push(templateParser(j.value));
+      // return templateParser(j.value);
+    })
+  );
+  return out;
 }
 
 // Parses template strings and returns data structures.
@@ -68,34 +70,40 @@ function templateParser(str) {
   let ogRE = /\[(.*?)\]/;
   let bracketRE = /[^[\]]+(?=])/g;
   let matches = str.match(bracketRE);
-  let out = { o: ogstr, f: "<final string>"}
+  let out = { o: ogstr, f: "<final string>" };
 
-	// set f (final string)
+  // set f (final string)
   if (matches == null) {
-		out["f"] = out.o
-		return out
-	}
+    out["f"] = out.o;
+    return out;
+  }
 
   matches.forEach(m => {
-    let matchCategory = m.slice(0, 1);  // ie, gets "t" from: "[t: 00:10:00]"
+    let matchCategory = m.slice(0, 1); // ie, gets "t" from: "[t: 00:10:00]"
     switch (matchCategory) {
       case "t":
         out["timer"] = m.slice(3, 11); // parse the timestring.
         out["f"] = str.replace(ogRE, "").trim(); // remover timer block.
-        return out
+        return out;
 
       case "#":
-        out["f"] = ogstr.replace(ogRE, (match) => {
-          let tagContent = match.split("|")[1].trim().slice(0, -1);
-          let tagId = match.split("|")[0].slice(3).trim()
-          return `<span data-quant="${tagId}" class="ingredient-tooltip">${tagContent}</span>`
+        out["f"] = ogstr.replace(ogRE, match => {
+          let tagContent = match
+            .split("|")[1]
+            .trim()
+            .slice(0, -1);
+          let tagId = match
+            .split("|")[0]
+            .slice(3)
+            .trim();
+          return `<span data-quant="${tagId}" class="ingredient-tooltip">${tagContent}</span>`;
         });
       default:
-				break;
+        break;
     }
-  })
+  });
 
-  return out
+  return out;
 }
 
 /**
@@ -110,53 +118,51 @@ function templateParser(str) {
  *
  */
 function tableParser(tableChildren) {
-  return tableChildren.reduce((acc, curr, idx) => {
-    // collect table column names as keys
-    if (idx === 0) {
-      tableChildren[0].children.forEach(key => {
-        acc["keys"].push(key.children[0].value);
-      })
-    }
+  return tableChildren.reduce(
+    (acc, curr, idx) => {
+      // collect table column names as keys
+      if (idx === 0) {
+        tableChildren[0].children.forEach(key => {
+          acc["keys"].push(key.children[0].value);
+        });
+      }
 
-    // skip if of type separator.
-    else if (curr.type === "table.separator") {
-      return acc
-    }
+      // skip if of type separator.
+      else if (curr.type === "table.separator") {
+        return acc;
+      } else {
+        let datNew = {};
+        // fill the object with cell data
+        curr.children.forEach((c, idx) => {
+          let currKey = acc["keys"][idx];
+          datNew[currKey] = c.children[0].value;
+        });
+        acc["data"].push(datNew);
+        return acc;
+      }
 
-    else {
-      let datNew = {}
-      // fill the object with cell data
-      curr.children.forEach((c, idx) => {
-        let currKey = acc["keys"][idx];
-        datNew[currKey] = c.children[0].value
-      })
-      acc["data"].push(datNew)
-      return acc
-    }
-
-    return acc;
-  }, { keys: [], data: [] })
+      return acc;
+    },
+    { keys: [], data: [] }
+  );
 }
-
 
 // Given a heading, this should return all the meta data for it.
 function getProperties(h) {
-  let headlineChildren = h.children[0].children // gets type "headline", which containers drawers.
+  let headlineChildren = h.children[0].children; // gets type "headline", which containers drawers.
   let props = headlineChildren.find(f => {
-    return f.type == "drawer" && f.name === "PROPERTIES"
-  })
+    return f.type == "drawer" && f.name === "PROPERTIES";
+  });
   return parseProperties(props.value);
 }
 
 function getLogbook(h) {
-  let headlineChildren = h.children[0].children // gets type "headline", which containers drawers.
+  let headlineChildren = h.children[0].children; // gets type "headline", which containers drawers.
   let logbook = headlineChildren.find(f => {
-    return f.type == "drawer" && f.name === "LOGBOOK"
-  })
+    return f.type == "drawer" && f.name === "LOGBOOK";
+  });
   return logbook;
 }
-
-
 
 /**
  *
@@ -172,19 +178,17 @@ function getLogbook(h) {
  ':rating: 5/5',
 */
 function parseProperties(p) {
-  let o = {}
+  let o = {};
   let vals = p.split("\n");
-  let keyMatch = /\:(.*?)\:/
+  let keyMatch = /\:(.*?)\:/;
 
   vals.forEach(v => {
     let key = v.split(":", 2);
     let val = v.replace(keyMatch, "");
-    o[key[1]] = val.trim()
-  })
-  return o
+    o[key[1]] = val.trim();
+  });
+  return o;
 }
 
 // current not available in orga-js
-function parseLogbook() {
-
-}
+function parseLogbook() {}
