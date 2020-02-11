ff9bd6c95897dd399db4b2062534f00c6fb54263
teesloane <tylersloane@gmail.com>
2020-02-10 20:03:58 -0500

    init search.

--- src/Pages/RecipeList.elm
+++ src/Pages/RecipeList.elm
@@ -4,6 +4,7 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Json.Decode as Decode
 import Pages.RecipeSingle exposing (Recipe, decodeRecipe)
+import Ui
 import Util
 
 
@@ -35,33 +36,43 @@ viewHero =
         ]
 
 
+viewSearch =
+    section [ class "section-search content" ]
+        [ input [ class "search-input" ] []
+        , button [ class "search-btn" ] [ text "Search" ]
+        ]
+
+
 view model =
     case model.recipes of
         Just recipes ->
             let
-                sortedRecipes = List.sortBy .belongs_to recipes
+                sortedRecipes =
+                    List.sortBy .belongs_to recipes
 
-                groupedRecipes = Util.groupWhileTransitively (\a b -> a.belongs_to == b.belongs_to) sortedRecipes
+                groupedRecipes =
+                    Util.groupWhileTransitively (\a b -> a.belongs_to == b.belongs_to) sortedRecipes
 
                 getCategoryName sec =
                     case List.head sec of
                         Just r ->
                             r.belongs_to ++ " - ish"
+
                         Nothing ->
                             ""
 
                 sectionList sec =
-                    div [] [
-                        h4 [class "recipe-category"] [text <| getCategoryName sec]
+                    div []
+                        [ h4 [ class "recipe-category" ] [ text <| getCategoryName sec ]
                         , ul [] (List.map rList sec)
                         ]
-
 
                 rList recipe =
                     li [] [ a [ href ("/recipe/" ++ recipe.slug) ] [ text recipe.name ] ]
             in
             section [ class "RecipeList" ]
                 [ viewHero
+                , viewSearch
                 , div [ class "columns" ] (List.map sectionList groupedRecipes)
                 ]
 
